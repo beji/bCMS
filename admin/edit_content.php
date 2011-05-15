3309b14299de6dd2bd962008aaebe2a7fcafcb19
beji <b.erlwein@gmx.de>
2011-05-15 22:00:23 +0200

    Added list of all categories to select on which categories the article should be displayed

--- admin/edit_content.php
+++ admin/edit_content.php
@@ -13,7 +13,6 @@ if(!isset($_GET['aid'])){
 	$log->writeErrorLog(__FILE__,__LINE__,"id not found.");
 }
 else{
-
 	if(!is_numeric($_GET['aid'])){
 		if (file_exists("./inc/log.php")) {$logpath="./inc/log.php";}
 		elseif (file_exists("../inc/log.php")) {$logpath="../inc/log.php";}
@@ -36,6 +35,33 @@ else{
 		else{
 			"NOT YET IMPLEMENTED";
 		}
+		//Get the categories the article is currently in
+		$getCurrentCats=mysql_query('SELECT  `'.DB_PREFIX.'categories`.`alias` 
+		    FROM  `'.DB_PREFIX.'cat_cont` 
+		    LEFT JOIN  `bcms`.`'.DB_PREFIX.'categories` ON  `'.DB_PREFIX.'cat_cont`.`cat_alias` =  `'.DB_PREFIX.'categories`.`alias` 
+		    WHERE (
+		    `'.DB_PREFIX.'cat_cont`.`cont_id` =1
+		    )
+		');
+		/*This line writes the whole sql result into a array instead of the one array per line you would get per mysql_fetch_array()
+		By KingIsulgard - http://php.net/manual/de/function.mysql-fetch-array.php*/
+		for($i = 0; $currentCats[$i] = mysql_fetch_assoc($getCurrentCats); $i++);
+		array_pop($currentCats); //Last one will always be emtpy so it can be deleted
+
+		//Now build a select-list of all categories with the already connected preselected
+		$getAllCategories=mysql_query('SELECT * FROM  `'.DB_PREFIX.'categories` ');
+		echo '<select name="categories" size="'.mysql_num_rows($getAllCategories).'">';   
+		while ($catList=mysql_fetch_array($getAllCategories)){
+		    $selected='';
+		    foreach($currentCats as $catToCheck){
+			if($catList['alias']==$catToCheck['alias']){
+			    $selected='selected';
+			}
+		    }
+		    echo '<option value="'.$catList['alias'].'" '.$selected.'>'.$catList['name'].'</option>';
+		}
+		echo '</select>';
+		echo '</form>';
 	}
 }
 ?>
